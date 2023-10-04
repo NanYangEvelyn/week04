25e42cf73fd0577eff457941cd46e0b855a31959
NanYangEvelyn <ny2187@nyu.edu>
2023-10-04 14:05:28 -0400

    Update sketch.js

--- 2/sketch.js
+++ 2/sketch.js
@@ -11,5 +11,5 @@ function draw() {
   background(255);
 let secondNow = second();
 let myDiameter = map(secondNow,0,59,100,height);
-ellipse(width / 2,height / 2, secondNow,secondNow);
+ellipse(width / 2,height / 2, myDiameter,myDiameter);
 }
